fe25b4d5531c829c12400e451cf5442d03e70a2b
blackear330 <blackgear333@gmail.com>
2026-03-14 01:50:30 +0500

    продолжаю писать AI логику принятия решений. Реализовал старый метод вычислений через простую математику и начал реализовывать новый метод через модификаторы.

--- Assets/Scripts/AILogic/DecisionMaker.cs
+++ Assets/Scripts/AILogic/DecisionMaker.cs
@@ -16,7 +16,7 @@ namespace AILogic
         Memory _memory;
         [SerializeField] private CharacterTraits _characterTraits;
         
-        void Start()
+        void Awake()
         {
             _states = GetComponent<States>();
             _memory = GetComponent<Memory>();
@@ -34,15 +34,46 @@ namespace AILogic
             float fightWeight = _characterTraits.bravery;
             float apathyWeight = _characterTraits.anxiety;
             float retreatWeight = _characterTraits.cowardice;
+            float randomFactor = UnityEngine.Random.Range(0.85f, 1.15f); // для генерации рандома.
             
-            
-            fightWeight +=  + (-_states.stress/100 -_states.fear/100 -_states.exhaustion/100);
-            float randomFactor = UnityEngine.Random.Range(0.85f, 1.15f);// для генерации рандома.
-            fightWeight *= randomFactor;
-            fightWeight = Mathf.Clamp(fightWeight, 0f, 100f) ; 
-            
-            Debug.Log($"fight chance {gameObject.name} {fightWeight}");
+            //TODO Модификаторы для Fight , пока что тест. (Переделать по плану, сделать общие множители для всех 3 вариантов "см. план реализации, шаг 2")
+            float stressMod = 1.0f - (_states.stress / 100f);
+            stressMod = Mathf.Max(stressMod, 0f);
+            float fearMod = 1.0f - (_states.fear / 100f);
+            fearMod = Mathf.Max(fearMod, 0f);
+            float exhaustMod = 1.0f - (_states.exhaustion / 100f);
+            exhaustMod = Mathf.Max(exhaustMod, 0f);
 
+            //TODO Расчет с модификаторами
+            fightWeight *= stressMod * fearMod * exhaustMod * randomFactor;
+
+            
+            Debug.Log($"Fight chanceMod {fightWeight}");
+            
+            
+            // TODO Расчет без модификаторов.
+            //Расчет вероятности боя
+            fightWeight +=  -_states.stress -_states.fear -_states.exhaustion;
+            fightWeight *= randomFactor;
+            fightWeight = Mathf.Clamp(fightWeight, 1f, 100f); 
+            
+            //расчет вероятности стазиса на 1-2 хода, сам стазис еще не реализован 
+            apathyWeight += (_states.exhaustion + _states.fear + _states.stress) - _characterTraits.bravery; 
+            apathyWeight *= randomFactor;
+            apathyWeight = Mathf.Clamp(apathyWeight, 1f, 100f);
+            
+            //расчет вероятности отступления
+            retreatWeight += (_states.exhaustion + _states.fear + _states.stress) - _characterTraits.bravery;
+            retreatWeight *= randomFactor;
+            retreatWeight = Mathf.Clamp(retreatWeight, 1f, 100f);
+            
+            // TODO далее должна быть нормализация
+            
+            Debug.Log($"fight chance {gameObject.name} {fightWeight}, храбрость {_characterTraits.bravery}, стресс {_states.stress}, страх {_states.fear}, усталость {_states.exhaustion}");
+            Debug.Log($"apathy chance {gameObject.name} {apathyWeight}, храбрость {_characterTraits.bravery}, стресс {_states.stress}, страх {_states.fear}, усталость {_states.exhaustion}");
+            Debug.Log($"retreat chance {gameObject.name} {retreatWeight}, храбрость {_characterTraits.bravery}, стресс {_states.stress}, страх {_states.fear}, усталость {_states.exhaustion}");
+            
+            //заглушка пока что
             return AIDecision.Fight;
         }
         
--- Assets/Scripts/AILogic/States.cs
+++ Assets/Scripts/AILogic/States.cs
@@ -8,7 +8,7 @@ namespace AILogic
     {
         [Header("Состояния персонажа (0-100)")]
         
-        [Range(0f, 100f)] public float stress = 0;
+        [Range(0f, 100f)] public float stress = 0 * 0.5f;
         [Range(0f, 100f)] public float fear = 0;
         [Range(0f, 100f)] public float exhaustion = 0;  // Усталость
 
